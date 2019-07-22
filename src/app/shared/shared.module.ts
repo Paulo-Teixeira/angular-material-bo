326c73700bcc10602bfabed78847207e21a5ad5c
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-22 16:03:02 +0200

    Implements shared model.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [CommonModule, FormsModule, MaterialModule]
+})
+export class SharedModule { }
