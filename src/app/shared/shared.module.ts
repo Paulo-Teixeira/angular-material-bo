f7d16c911d6748d3f6e603d5584b53b5d3fbb6e9
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-23 10:36:34 +0200

    Moves Material module to root of shared folder.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   imports: [CommonModule],
