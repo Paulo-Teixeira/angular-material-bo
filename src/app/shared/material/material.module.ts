24062573d0e768627eecc06ddc366b46bec67871
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-23 10:23:15 +0200

    Adds Sidenav module.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule]
+  imports: [
+    MatButtonModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatSidenavModule
+  ]
 })
 export class MaterialModule { }
