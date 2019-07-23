ead4d12a79860e42eaff2b656d01a9c021742c44
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-23 11:39:26 +0200

    Lazy load bookings module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then(mod => mod.BookingsModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
