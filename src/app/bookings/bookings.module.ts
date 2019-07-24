29d6cf1d91ee8c39a8150ab8942f184c636370a7
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-24 11:06:28 +0200

    Adds presentation for toolbar component.

--- src/app/bookings/bookings.module.ts
+++ src/app/bookings/bookings.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { BookingsRoutingModule } from './bookings-routing.module';
+import { BookingsComponent } from './bookings.component';
 import { BookingToolbarComponent } from './booking-toolbar/booking-toolbar.component';
 import { BookingFormComponent } from './booking-form/booking-form.component';
-import { BookingsComponent } from './bookings.component';
 
 
 @NgModule({
