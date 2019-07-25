604242f8f0ad335c43255a30838f1710da4be782
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-25 12:57:24 +0200

    Updates Material module.

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
+    MatRadioModule,
     MatSidenavModule,
     MatToolbarModule
   ],
   exports: [
     MatButtonModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
+    MatRadioModule,
     MatSidenavModule,
     MatToolbarModule
   ]
