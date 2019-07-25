e11d8f67b8bb305bbea11b8f175fbccbf9ec063d
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-25 11:32:28 +0200

    Removes test routes from bookings module router.

--- src/app/bookings/bookings-routing.module.ts
+++ src/app/bookings/bookings-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BookingsComponent } from './bookings.component';
-import { BookingToolbarComponent } from './booking-toolbar/booking-toolbar.component';
-import { BookingFormComponent } from './booking-form/booking-form.component';
 
 
 const routes: Routes = [
@@ -11,14 +9,6 @@ const routes: Routes = [
     path: '',
     component: BookingsComponent
   },
-  {
-    path: 'toolbar',
-    component: BookingToolbarComponent
-  },
-  {
-    path: 'form',
-    component: BookingFormComponent
-  }
 ];
 
 @NgModule({
