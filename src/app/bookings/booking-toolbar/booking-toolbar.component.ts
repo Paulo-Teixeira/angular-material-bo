fc38e458bdf9f0727cae7186d79935451c5424fa
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-07-29 12:08:29 +0200

    Sets input and output values for toolbar component.

--- src/app/bookings/booking-toolbar/booking-toolbar.component.ts
+++ src/app/bookings/booking-toolbar/booking-toolbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-booking-toolbar',
   templateUrl: './booking-toolbar.component.html',
   styleUrls: ['./booking-toolbar.component.scss']
 })
-export class BookingToolbarComponent implements OnInit {
+export class BookingToolbarComponent {
+
+  // Form component sends form validity information
+  @Input() isFormValid: boolean;
+
+  // Sens click event to form component to validate de form
+  @Output() formSubmited = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
+  submitForm() {
+    this.formSubmited.emit();
   }
 
 }
