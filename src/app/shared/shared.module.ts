089ca99f4e943f7aac0baeec6cd8f2d1d768df26
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-08-05 15:55:02 +0200

    Loads form cleaner service in shared module.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
+import { FormCleanerService } from './form-cleaner.service';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule]
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  providers: [FormCleanerService]
 })
 export class SharedModule { }
