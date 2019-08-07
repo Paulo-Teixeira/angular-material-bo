8128aedc86b34de18587e4ed4668608df940cf16
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-08-07 16:46:10 +0200

    Renames slide toggle variables.

--- src/app/bookings/booking-form/booking-form.component.ts
+++ src/app/bookings/booking-form/booking-form.component.ts
@@ -53,20 +53,20 @@ export class BookingFormComponent implements OnInit {
   }
 
   toggleIda(toggle: MatSlideToggle): void {
-    const relatedInputs = [this.bookingsForm.get('codeOne'), this.bookingsForm.get('codeTwo')];
-    this.toggleHelper(toggle, relatedInputs);
+    const siblingInputs = [this.bookingsForm.get('codeOne'), this.bookingsForm.get('codeTwo')];
+    this.toggleHelper(toggle, siblingInputs);
   }
 
   toggleIdb(toggle: MatSlideToggle): void {
-    const relatedInputs = [this.bookingsForm.get('codeAlpha'), this.bookingsForm.get('codeBravo')];
-    this.toggleHelper(toggle, relatedInputs);
+    const siblingInputs = [this.bookingsForm.get('codeAlpha'), this.bookingsForm.get('codeBravo')];
+    this.toggleHelper(toggle, siblingInputs);
   }
 
-  private toggleHelper(toggle, ...inputs): void {
+  private toggleHelper(toggle: MatSlideToggle, ...selectedInputs): void {
     if (toggle.checked) {
-      inputs[0].forEach(input => input.enable());
+      selectedInputs[0].forEach(input => input.enable());
     } else {
-      inputs[0].forEach(input => input.disable());
+      selectedInputs[0].forEach(input => input.disable());
     }
   }
 }
