a2bb5f3ac00a9b501f18e86b53352973323f5586
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-08-12 16:39:16 +0200

    Adds basic unit testing for bookings form.

--- src/app/bookings/booking-form/booking-form.component.spec.ts
+++ src/app/bookings/booking-form/booking-form.component.spec.ts
@@ -1,10 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { SharedModule } from '../../shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { SharedModule } from '../../shared/shared.module';
 import { BookingFormComponent } from './booking-form.component';
+
+import 'hammerjs';
 
 describe('BookingFormComponent', () => {
   let component: BookingFormComponent;
@@ -27,5 +27,32 @@ describe('BookingFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when required inputs are empty', () => {
+    expect(component.bookingsForm.valid).toBeFalsy();
+  });
+
+  it('booking type should have a set default value', () => {
+    let bookingType = component.bookingsForm.controls['bookingType'];
+    expect(bookingType.value).toBeTruthy();
+  });
+
+  it('payment type should have a set default value', () => {
+    let paymentType = component.bookingsForm.controls['paymentType'];
+    expect(paymentType.value).toBeTruthy();
+  });
+
+  it('submiting a form should emit the form data', () => {
+    expect(component.bookingsForm.valid).toBeFalsy();
+
+    const requiredFields = ['clientReference', 'clientBooker', 'transitLabel','documents'];
+
+    // Set the values for the required form fields
+    requiredFields.forEach((value, index) => {
+      component.bookingsForm.controls[value].setValue(`value ${index}`);
+    });
+    
+    expect(component.bookingsForm.valid).toBeTruthy();
   });
 });
