62527a01564f9567e61539d6f335115bf929ee1d
Paulo-Teixeira <paulo.jtfs@gmail.com>
2019-09-19 16:39:03 +0200

    Removes form control import

--- src/app/bookings/booking-form/booking-form.component.ts
+++ src/app/bookings/booking-form/booking-form.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
